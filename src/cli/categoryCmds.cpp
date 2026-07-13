98c7cfa55d3a0f63080b854946a989923d956729
Grady Miller <182833936+gradymiller@users.noreply.github.com>
2026-07-13 14:52:42 -0400

    started category commands, added categoryAdd and categoryDelete

--- src/cli/categoryCmds.cpp
+++ src/cli/categoryCmds.cpp
@@ -1,18 +1,44 @@
 #include <iostream>
+#include <stdexcept>
+#include "budget.hpp"
 
 
 int categoryAdd(int argc, char** argv) {
-	return 0;
+	Budget budget;
+	budget.load();
+
+	if (argc < 1) {
+		throw std::invalid_argument("Too few arguments");
+	}
+
+	budget.addCategory(argv[0]);	
+	budget.save();
 }
 
 int categoryEdit(int argc, char** argv) {
+	Budget budget;
+	budget.load();
 	return 0;
 }
 
 int categoryDelete(int argc, char** argv) {
+	Budget budget;
+	budget.load();
+
+	if (argc < 1) {
+		throw std::invalid_argument("Too few arguments");
+	}
+
+	budget.delCategory(argv[0]);	
+	budget.save();
 	return 0;
 }
 
 int categoryList(int argc, char** argv) {
+	Budget budget;
+	budget.load();
+
+	budget.getCategories
+
 	return 0;
 }
