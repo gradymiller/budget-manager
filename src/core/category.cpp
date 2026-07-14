760feeed0b4dedd2b16eb31ec1646771a9dc2bbd
Grady Miller <182833936+gradymiller@users.noreply.github.com>
2026-07-14 15:34:56 -0400

    Added a basic category class, switching from a list to a classes that have different characteristics rather than just a name, e.g. a category limit

---
+++ src/core/category.cpp
@@ -0,0 +1,23 @@
+// TODO: Implement all these
+#include "Category.h"
+
+const std::string& Category::getName() const {
+	return name;
+}
+
+CategoryType Category::getType() const {
+	return type;
+}
+
+double Category::getLimit() const {
+	return limit;
+}
+
+void Category::setName(const std::string& name) {
+}
+
+void Category::setType(const std::string& type) {
+}
+
+void Category::setLimit(const std::string& limit) {
+}
---
+++ src/core/category.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+
+enum class CategoryType {
+	Income,
+	Expense
+};
+
+class Category {
+public:
+	Category() = default;
+
+	const std::string& getName() const;
+	CategoryType getType() const;
+	double getLimit() const;
+
+	void setName(const std::string& name);
+	void setType(const std::string& type);
+	void setLimit(const std::string& limit);
+
+private:
+	std::string name;
+	CategoryTYpe type = CategoryType::Expense;
+	double limit = 0.0;
+}
