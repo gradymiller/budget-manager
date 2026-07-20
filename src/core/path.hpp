aad6db4189afd0147d73a513afffa653869ad691
Grady Miller <182833936+gradymiller@users.noreply.github.com>
2026-07-20 17:51:59 -0400

    Commented core/path header

--- src/core/path.hpp
+++ src/core/path.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
-#include <filesystem>
 #include <cstdlib>
+#include <filesystem>
 
+// This is the main path used throughout the program
+// TODO: Make this editable from a config file
 inline std::filesystem::path PATH =
     std::filesystem::path(std::getenv("HOME")) /
     ".local/share/budget-manager";
