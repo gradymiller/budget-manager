223bc4d3bb24016705d3b404fccc67bc0ac3cd56
Grady Miller <182833936+gradymiller@users.noreply.github.com>
2026-07-24 15:02:58 -0400

    setup the basic functions that are needed, setup their implementations so it could compile

--- src/core/database.cpp
+++ src/core/database.cpp
@@ -4,19 +4,89 @@
 #include <stdexcept>
 #include <string>
 
-Database::Database(const std::string& filename) {
-	if (sqlite3_open(filename.c_str(), &db) != SQLITE_OK) {
-		std::string error = sqlite3_errmsg(db);
-		sqlite3_close(db);
-		db = nullptr;
 
-		throw std::runtime_error("Database Error: " + error);
-	}	
+Database::Database(const std::string& filename) {
+    if (sqlite3_open(filename.c_str(), &db) != SQLITE_OK) {
+        std::string error = sqlite3_errmsg(db);
+        sqlite3_close(db);
+        db = nullptr;
+
+        throw std::runtime_error("Database error: " + error);
+    }
 }
 
 Database::~Database() {
-	if (db != nullptr) {
-		sqlite3_close(db);
-		db = nullptr;
-	}
+    if (db != nullptr) {
+        sqlite3_close(db);
+        db = nullptr;
+    }
 }
+
+
+void Database::createTables() {
+    // TODO: create SQL tables here
+}
+
+
+void Database::addBudget() {
+    // TODO
+}
+
+
+void Database::editBudget() {
+    // TODO
+}
+
+
+void Database::deleteBudget() {
+    // TODO
+}
+
+
+void Database::getBudgets() {
+    // TODO
+}
+
+
+void Database::addCategory() {
+    // TODO
+}
+
+
+void Database::editCategory() {
+    // TODO
+}
+
+
+void Database::deleteCategory() {
+    // TODO
+}
+
+
+std::vector<Category> Database::getCategories() {
+    // TODO: query categories
+    return {};
+}
+
+
+void Database::addTransaction() {
+    // TODO
+}
+
+
+void Database::editTransaction() {
+    // TODO
+}
+
+
+void Database::deleteTransaction() {
+    // TODO
+}
+
+
+std::vector<Transaction> Database::getTransactions(
+	const TransactionFilter& filter) {
+    // TODO: build SELECT query using filter
+    return {};
+}
+
--- src/core/database.hpp
+++ src/core/database.hpp
@@ -1,13 +1,50 @@
 #pragma once
 
+#include <optional>
 #include <string>
+#include <vector>
 
 #include <sqlite3.h>
+
+#include "core/budget.hpp"
+#include "core/category.hpp"
+#include "core/transaction.hpp"
+
+struct TransactionFilter {
+	std::optional<int> id;
+	std::optional<int> categoryId;
+	std::optional<int> budgetID;
+
+	std::optional<std::string> vendor;
+
+	std::optional<std::string> start_date;
+	std::optional<std::string> end_date;
+};
 
 class Database {
 public:
 	Database(const std::string& filename);
 	~Database();
+
+	void createTables();
+
+	void addBudget();
+	void editBudget();
+	void deleteBudget();
+	void getBudgets();
+
+	void addCategory();
+	void editCategory();
+	void deleteCategory();
+	std::vector<Category> getCategories();
+
+	void addTransaction();
+	void editTransaction();
+	void deleteTransaction();
+	std::vector<Transaction> getTransactions(
+		const TransactionFilter& filter = {}	
+	);
+
 
 private:
 	sqlite3* db = nullptr;
